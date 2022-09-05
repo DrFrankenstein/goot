1c7ba1fc1fbe48d5da3436b839cd138f8aa8f2e3
Carl Tessier <doktorfrankenstein@gmail.com>
2022-09-05 14:15:53 -0400

    InitWizard: fix some summary page things
    - properly show the repo name in the page subtitle
    - show the default branch name "master" when none is selected

--- src/goot/Gui/InitWizard/Page4Summary.cpp
+++ src/goot/Gui/InitWizard/Page4Summary.cpp
@@ -16,12 +16,18 @@ auto Page4Summary::initializePage() -> void
 {
 	const auto format = ui.labelSummary->text();
 	const auto repo = field(InitWizard::Fields::path.toString()).toString();
-	const auto ref = field(InitWizard::Fields::ref.toString()).toString();
+	auto ref = field(InitWizard::Fields::ref.toString()).toString();
 	const auto customWorkdir = field(InitWizard::Fields::customWorkdir.toString()).toBool();
 
 	const auto workdir = customWorkdir ?
 	    field(InitWizard::Fields::workdir.toString()).toString() :
 	    repo;
+
+	if (ref.isEmpty())
+		ref = QStringLiteral("master");
+
+	const auto subTitleFormat = subTitle();
+	setSubTitle(subTitleFormat.arg(repo));
 
 	ui.labelSummary->setText(format.arg(repo, ref, workdir));
 }
