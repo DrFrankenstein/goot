fbb5ef87a34eb945f230b86ae26c60971df4b485
Carl Tessier <doktorfrankenstein@gmail.com>
2022-09-10 10:49:48 -0400

    feat(git2xx): add OidShortener (#22)
    Some of the static functions to create Oid objects have also been moved
    to constructors.

--- src/libgit2xx/git2xx/Oid.hpp
+++ src/libgit2xx/git2xx/Oid.hpp
@@ -3,15 +3,35 @@
 #include "Error.hpp"
 
 #include <cstddef>
+#include <exception>
 #include <git2/oid.h>
+#include <ranges>
 #include <string>
 #include <string_view>
+#include <utility>
 
 namespace Git
 {
 
 struct Oid : git_oid
 {
+	Oid()
+	{
+		std::ranges::fill(id, unsigned char {});
+	}
+
+	Oid(std::string_view str)
+	{
+		const auto status = git_oid_fromstrn(this, str.data(), str.size());
+		ensureOk(status);
+	}
+
+	Oid(const unsigned char* raw)
+	{
+		const auto status = git_oid_fromraw(this, raw);
+		ensureOk(status);
+	}
+
 	auto operator==(const Oid& other) const noexcept
 	{
 		return git_oid_equal(this, &other);
@@ -41,24 +61,46 @@ struct Oid : git_oid
 
 		return result;
 	}
-
-	static auto fromString(std::string_view str)
-	{
-		Oid oid;
-		const auto status = git_oid_fromstrn(&oid, str.data(), str.size());
-		ensureOk(status);
-
-		return oid;
-	}
-
-	static auto fromRaw(const unsigned char* raw)
-	{
-		Oid oid;
-		const auto status = git_oid_fromraw(&oid, raw);
-		ensureOk(status);
-
-		return oid;
-	}
 };
 
+class OidShortener
+{
+	public:
+	OidShortener(std::size_t min_length):
+	    m_shortener { git_oid_shorten_new(min_length) }
+	{
+		if (m_shortener == nullptr)
+		{
+			throw std::bad_alloc {};
+		}
+	}
+
+	~OidShortener()
+	{
+		git_oid_shorten_free(m_shortener);
+	}
+
+	OidShortener(const OidShortener& other) = delete;
+	auto operator=(const OidShortener other) -> OidShortener& = delete;
+
+	OidShortener(OidShortener&& other) noexcept:
+	    m_shortener { std::exchange(other.m_shortener, nullptr) }
+	{ }
+	auto operator=(OidShortener& other) noexcept -> OidShortener&
+	{
+		std::swap(m_shortener, other.m_shortener);
+		return *this;
+	}
+
+	auto add(const std::string& text_id)
+	{
+		const auto result = git_oid_shorten_add(m_shortener, text_id.c_str());
+		ensureOk(result);
+
+		return result;
+	}
+
+	private:
+	git_oid_shorten* m_shortener;
+};
 }
