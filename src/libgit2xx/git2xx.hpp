89205640ccc78567d9ed4a9a39e0c011ec346047
Carl Tessier <doktorfrankenstein@gmail.com>
2022-09-26 20:47:22 -0400

    chore: precompile headers (#24)
    Compiling a set of headers ahead of time results in a massive speedup in
    compilation times, as those headers only need to be read, preprocessed,
    and parsed once.
    
    With these changes, most C++ files only take a couple hundred
    milliseconds to compile.
    
    Resolves #23.

--- src/libgit2xx/git2xx.hpp
+++ src/libgit2xx/git2xx.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "git2xx/Error.hpp"
 #include "git2xx/Buffer.hpp"
-#include "git2xx/StrArray.hpp"
+#include "git2xx/Error.hpp"
 #include "git2xx/Git.hpp"
+#include "git2xx/Oid.hpp"
 #include "git2xx/Repository.hpp"
+#include "git2xx/StrArray.hpp"
